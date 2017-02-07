0dea05fed283a5d5e2df8744314d501874284d32
andrewdyer <andrewdyer@10.0.0.6>
2017-02-07 22:04:42 +0000

    Added database structure and inserts

---
+++ _install/db-structure.sql
@@ -0,0 +1,13 @@
+--
+-- Table structure for table 'users'
+--
+CREATE TABLE IF NOT EXISTS `users` (
+    `id` BIGINT(20) NOT NULL AUTO_INCREMENT,
+    `email` VARCHAR(254) NOT NULL,
+    `forename` VARCHAR(100) NOT NULL,
+    `password` TEXT NOT NULL,
+    `salt` TEXT NOT NULL,
+    `surname` VARCHAR(100) NOT NULL,
+    `username` VARCHAR(32) NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=INNODB DEFAULT CHARSET=utf8;
