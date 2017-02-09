17adeb0c352518e1d6b551db4c42289006e0c273
Andrew <contact@andrewdyer.rocks>
2017-02-09 14:47:25 +0000

    Fixed an issue where the server protocol was hard coded to be http

--- www/app/init.php
+++ www/app/init.php
@@ -2,13 +2,16 @@
 
 require_once "../../vendor/autoload.php";
 
+
+
 // Config
 define("ROOT", realpath(dirname(__FILE__) . "/../") . "/");
 
 // App Config
 define("APP_NAME", "myApp");
 define("APP_ROOT", ROOT . "app/");
-define("APP_URL", "http://" . $_SERVER['HTTP_HOST'] . str_replace('public', '', dirname($_SERVER['SCRIPT_NAME'])) . "/");
+define("APP_PROTOCOL", stripos($_SERVER["SERVER_PROTOCOL"], "https") === true ? "https://" : "http://");
+define("APP_URL", APP_PROTOCOL . $_SERVER["HTTP_HOST"] . str_replace("public", "", dirname($_SERVER["SCRIPT_NAME"])) . "/");
 define("APP_CONFIG_FILE", APP_ROOT . "config.php");
 
 // Public Config
@@ -22,7 +25,7 @@ define("DEFAULT_CONTROLLER_ACTION", "index");
 // View Config
 define("VIEW_PATH", "../app/View/");
 define("DEFAULT_HEADER_PATH", "_template/header");
-define("DEFAULT_FOOTER_PATH",  "_template/footer");
+define("DEFAULT_FOOTER_PATH", "_template/footer");
 define("HTMLENTITIES_FLAGS", ENT_QUOTES);
 define("HTMLENTITIES_ENCODING", "UTF-8");
 define("HTMLENTITIES_DOUBLE_ENCODE", false);
