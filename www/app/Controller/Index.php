75ea92b8e2df4e354fbc115b3ca29519a573beeb
Andrew <contact@andrewdyer.rocks>
2017-02-09 14:00:10 +0000

    Updated the index controller to only be accessible to logged in users. Output some of of user's info in the view.

--- www/app/Controller/Index.php
+++ www/app/Controller/Index.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Core;
 use App\Model;
+use App\Utility;
 
 /**
  * Index Controller:
@@ -18,15 +19,25 @@ class Index extends Core\Controller {
      * @access public
      * @example index/index
      * @return void
+     * @since 1.0
      */
     public function index() {
-        
+
+        // Check that the user is authenticated.
+        Utility\Auth::checkAuthenticated();
+
+        // Get an instance of the user model using the ID stored in the session. 
+        $userID = Utility\Session::get(Utility\Config::get("SESSION_USER"));
+        if (!$User = Model\User::getInstance($userID)) {
+            Utility\Redirect::to(404);
+        }
+
         // Set any dependencies, data and render the view.
         $this->View->addCSS("css/index.css");
         $this->View->addJS("js/index.jquery.js");
         $this->View->render("index/index", [
-            "title" => "Index Controller",
-            "songs" => Model\Index::getSongs()
+            "title" => "Index",
+            "user" => $User->data()
         ]);
     }
 
