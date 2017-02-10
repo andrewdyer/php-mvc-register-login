593a672dcd38359018b72f3e89d47ba56c2d7491
Andrew <contact@andrewdyer.rocks>
2017-02-10 16:55:16 +0000

    Restored the .htaccess file in the app dir.

--- www/app/init.php
+++ www/app/init.php
@@ -9,7 +9,7 @@ define("ROOT", realpath(dirname(__FILE__) . "/../") . "/");
 define("APP_NAME", "myApp");
 define("APP_ROOT", ROOT . "app/");
 define("APP_PROTOCOL", stripos($_SERVER["SERVER_PROTOCOL"], "https") === true ? "https://" : "http://");
-define("APP_URL", APP_PROTOCOL . $_SERVER["HTTP_HOST"] . str_replace("public", "", dirname($_SERVER["SCRIPT_NAME"])) . "/");
+define("APP_URL", APP_PROTOCOL . $_SERVER["HTTP_HOST"] . str_replace("public_html", "", dirname($_SERVER["SCRIPT_NAME"])) . "/");
 define("APP_CONFIG_FILE", APP_ROOT . "config.php");
 
 // Public Config
