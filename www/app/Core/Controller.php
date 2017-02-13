597cbcb6d7ac995ed6f168129522908c697bbdf5
Andrew <contact@andrewdyer.rocks>
2017-02-13 14:14:37 +0000

    Updated Core/Controller so that it initalizes a session and attempts to login in the user if a remember me cookie has been set.

--- www/app/Core/Controller.php
+++ www/app/Core/Controller.php
@@ -2,6 +2,8 @@
 
 namespace App\Core;
 
+use App\Utility;
+
 /**
  * Core Controller:
  *
@@ -20,6 +22,23 @@ class Controller {
      * @since 1.0
      */
     public function __construct() {
+
+        // Initialize a session.
+        Utility\Session::init();
+
+        // If the user is not logged in but a remember cookie exists then
+        // attempt to login with cookie. NOTE: We only do this if we are not on
+        // the login with cookie controller method (this avoids creating an
+        // infinite loop).
+        if (Utility\Input::get("url") !== "login/_loginWithCookie") {
+            $cookie = Utility\Config::get("COOKIE_USER");
+            $session = Utility\Config::get("SESSION_USER");
+            if (!Utility\Session::exists($session) and Utility\Cookie::exists($cookie)) {
+                Utility\Redirect::to(APP_URL . "login/_loginWithCookie");
+            }
+        }
+
+        // Create a new instance of the core view class.
         $this->View = new View;
     }
 
